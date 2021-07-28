72250df6be4f43cbeac94b6dc8936e1fa469d628
owel <rwlllaguitan@yahoo.com>
2021-07-28 12:20:10 +0800

    Initial commit for Basic Maven lesson

---
+++ src/test/java/AppiumTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+    @Test
+    public void NativeAppAndroid(){
+        System.out.println("NativeAppAndroid");
+    }
+
+    @Test
+    public void ISOApps(){
+        System.out.println("ISOApps");
+    }
+}
---
+++ src/test/java/RESTApiTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class RESTApiTest {
+
+    @Test
+    public void postJIRA(){
+        System.out.println("postJIRA");
+    }
+
+    @Test
+    public void deleteTwitter(){
+        System.out.println("deleteTwitter");
+    }
+}
